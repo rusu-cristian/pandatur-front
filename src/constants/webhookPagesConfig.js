82d493380f22bddb8169ca1982125385a608d9a3
maksimbordan <bordan.maxim@gmail.com>
2025-11-04 11:56:25 +0200

    fix masiv ref

--- src/constants/webhookPagesConfig.js
+++ src/constants/webhookPagesConfig.js
@@ -4,127 +4,127 @@ export const WEBHOOK_PAGES_CONFIG = {
       page_id: "142678122441525",
       page_name: "Panda Tur MD",
       type: "facebook",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "110499316273706",
       page_name: "Panda Tur Chisinau",
       type: "facebook",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "583828601488213",
       page_name: "Oferte Pandatur",
       type: "facebook",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "671031122758947",
       page_name: "Rascani",
       type: "facebook",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "520045481191100",
       page_name: "Drochia",
       type: "facebook",
-      group_title: "DROCHIA"
+      group_title: ["DROCHIA"]
     },
     {
       page_id: "554007254472079",
       page_name: "Soldanesti",
       type: "facebook",
-      group_title: "SOLDANESTI"
+      group_title: ["SOLDANESTI"]
     },
     {
       page_id: "526796067184543",
       page_name: "Edinet",
       type: "facebook",
-      group_title: "EDINET"
+      group_title: ["EDINET"]
     },
     {
       page_id: "613702355151882",
       page_name: "Buiucani",
       type: "facebook",
-      group_title: "BUIUCANI"
+      group_title: ["BUIUCANI"]
     },
     {
       page_id: "605095636016314",
       page_name: "Ialoveni",
       type: "facebook",
-      group_title: "IALOVENI"
+      group_title: ["IALOVENI"]
     },
     {
       page_id: "573580082513912",
       page_name: "Causeni",
       type: "facebook",
-      group_title: "CAUSENI"
+      group_title: ["CAUSENI"]
     },
     {
       page_id: "696508703534741",
       page_name: "Orhei",
       type: "facebook",
-      group_title: "ORHEI"
+      group_title: ["ORHEI"]
     },
     {
       page_id: "642015418986129",
       page_name: "Rezina",
       type: "facebook",
-      group_title: "REZINA"
+      group_title: ["REZINA"]
     },
     {
       page_id: "775542898973257",
       page_name: "Cimislia",
       type: "facebook",
-      group_title: "CIMISLIA"
+      group_title: ["CIMISLIA"]
     },
     {
       page_id: "750746644794910",
       page_name: "Hancesti",
       type: "facebook",
-      group_title: "HANCESTI"
+      group_title: ["HANCESTI"]
     },
     {
       page_id: "789625717563101",
       page_name: "Stauceni",
       type: "facebook",
-      group_title: "STAUCENI"
+      group_title: ["STAUCENI"]
     },
     {
       page_id: "712585955262361",
       page_name: "Nisporeni",
       type: "facebook",
-      group_title: "NISPORENI"
+      group_title: ["NISPORENI"]
     },
     {
       page_id: "713252391860655",
       page_name: "Cantemir",
       type: "facebook",
-      group_title: "CANTEMIR"
+      group_title: ["CANTEMIR"]
     },
     {
       page_id: "103418101475044",
       page_name: "Comrat",
       type: "facebook",
-      group_title: "COMRAT"
+      group_title: ["COMRAT"]
     },
     {
       page_id: "130361096818926",
       page_name: "Panda Tour Bucuresti",
       type: "facebook",
-      group_title: "RO"
+      group_title: ["RO"]
     },
     {
       page_id: "150705011449556",
       page_name: "Panda Tour Romania",
       type: "facebook",
-      group_title: "RO"
+      group_title: ["RO"]
     },
     {
       page_id: "172917456820528",
       page_name: "Panda Tour Iasi",
       type: "facebook",
-      group_title: "RO"
+      group_title: ["RO"]
     }
   ],
   instagrams: [
@@ -132,97 +132,97 @@ export const WEBHOOK_PAGES_CONFIG = {
       page_id: "17841474040991649",
       page_name: "Panda Tur MD",
       type: "instagram",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "17841473358875325",
       page_name: "Oferte Pandatur",
       type: "instagram",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "17841475266910359",
       page_name: "Rascani",
       type: "instagram",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "17841473664962072",
       page_name: "Drochia",
       type: "instagram",
-      group_title: "DROCHIA"
+      group_title: ["DROCHIA"]
     },
     {
       page_id: "17841473802957787",
       page_name: "Soldanesti",
       type: "instagram",
-      group_title: "SOLDANESTI"
+      group_title: ["SOLDANESTI"]
     },
     {
       page_id: "17841473805179062",
       page_name: "Edinet",
       type: "instagram",
-      group_title: "EDINET"
+      group_title: ["EDINET"]
     },
     {
       page_id: "17841472932273948",
       page_name: "Buiucani",
       type: "instagram",
-      group_title: "BUIUCANI"
+      group_title: ["BUIUCANI"]
     },
     {
       page_id: "17841472760225651",
       page_name: "Ialoveni",
       type: "instagram",
-      group_title: "IALOVENI"
+      group_title: ["IALOVENI"]
     },
     {
       page_id: "17841473531937584",
       page_name: "Causeni",
       type: "instagram",
-      group_title: "CAUSENI"
+      group_title: ["CAUSENI"]
     },
     {
       page_id: "17841474362476129",
       page_name: "Orhei",
       type: "instagram",
-      group_title: "ORHEI"
+      group_title: ["ORHEI"]
     },
     {
       page_id: "17841473585604688",
       page_name: "Rezina",
       type: "instagram",
-      group_title: "REZINA"
+      group_title: ["REZINA"]
     },
     {
       page_id: "17841474097621521",
       page_name: "Cantemir",
       type: "instagram",
-      group_title: "CANTEMIR"
+      group_title: ["CANTEMIR"]
     },
     {
       page_id: "17841461747788266",
       page_name: "Panda Tour Bucuresti",
       type: "instagram",
-      group_title: "RO"
+      group_title: ["RO"]
     },
     {
       page_id: "17841407499908167",
       page_name: "Panda Tour Romania",
       type: "instagram",
-      group_title: "RO"
+      group_title: ["RO"]
     },
     {
       page_id: "17841459583103605",
       page_name: "Panda Tour Brasov",
       type: "instagram",
-      group_title: "RO"
+      group_title: ["RO"]
     },
     {
       page_id: "17841462950750247",
       page_name: "Panda Tour Iasi",
       type: "instagram",
-      group_title: "RO"
+      group_title: ["RO"]
     }
   ],
   whatsapps: [
@@ -230,49 +230,49 @@ export const WEBHOOK_PAGES_CONFIG = {
       page_id: "37360991919",
       page_name: "Chisinau - 37360991919",
       type: "phone",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "37369440400",
       page_name: "Chisinau - 37369440400",
       type: "phone",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "37361161919",
       page_name: "Rascani - 37361161919",
       type: "phone",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "37361001919",
       page_name: "Rascani - 37361001919",
       type: "phone",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "37361141919",
       page_name: "Drochia - 37361141919",
       type: "phone",
-      group_title: "DROCHIA"
+      group_title: ["DROCHIA"]
     },
     {
       page_id: "40720949119",
       page_name: "Iasi - 40720949119",
       type: "phone",
-      group_title: "RO"
+      group_title: ["RO"]
     },
     {
       page_id: "40728932931",
       page_name: "Bucuresti - 40728932931",
       type: "phone",
-      group_title: "RO"
+      group_title: ["RO"]
     },
     {
       page_id: "40721205105",
       page_name: "Brasov - 40721205105",
       type: "phone",
-      group_title: "RO"
+      group_title: ["RO"]
     }
   ],
   vibers: [
@@ -280,31 +280,31 @@ export const WEBHOOK_PAGES_CONFIG = {
       page_id: "37360991919",
       page_name: "Chisinau - 37360991919",
       type: "phone",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     {
       page_id: "37361161919",
       page_name: "Rascani - 37361161919",
       type: "phone",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "37361001919",
       page_name: "Rascani - 37361001919",
       type: "phone",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "37361141919",
       page_name: "Drochia - 37361141919",
       type: "phone",
-      group_title: "DROCHIA"
+      group_title: ["DROCHIA"]
     },
     {
       page_id: "380984284284",
       page_name: "Catalan - 380984284284",
       type: "phone",
-      group_title: "CATALAN"
+      group_title: ["CATALAN"]
     }
   ],
   "viber-bots": [
@@ -312,7 +312,7 @@ export const WEBHOOK_PAGES_CONFIG = {
       page_id: "viber-bot-pandatur",
       page_name: "Chisinau - viber-bot-pandatur",
       type: "viber-bot",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     }
   ],
   telegrams: [
@@ -320,7 +320,7 @@ export const WEBHOOK_PAGES_CONFIG = {
       page_id: "37360991919",
       page_name: "Chisinau - 37360991919",
       type: "telegram",
-      group_title: "MD"
+      group_title: ["MD", "HR", "Marketing", "Individual groups", "Business groups", "Franchise", "Agency"]
     },
     // {
     //   page_id: "37360361919",
@@ -332,25 +332,25 @@ export const WEBHOOK_PAGES_CONFIG = {
       page_id: "37361001919",
       page_name: "Rascani - 37361001919",
       type: "telegram",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "37361161919",
       page_name: "Rascani - 37361161919",
       type: "telegram",
-      group_title: "RASCANI"
+      group_title: ["RASCANI"]
     },
     {
       page_id: "37361141919",
       page_name: "Drochia - 37361141919",
       type: "telegram",
-      group_title: "DROCHIA"
+      group_title: ["DROCHIA"]
     },
     {
       page_id: "380984284284",
       page_name: "Catalan - 380984284284",
       type: "telegram",
-      group_title: "CATALAN"
+      group_title: ["CATALAN"]
     }
   ]
 };
